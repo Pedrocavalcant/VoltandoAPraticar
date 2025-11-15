52da0834a54324e24ad4f6ba31d4b57d78ddf8b6
Pedrocavalcant <pedrocavalcante50256@gmail.com>
2025-11-15 09:14:28 -0300

    Add project files.

---
+++ VoltandoAPraticar/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace VoltandoAPraticar;
+
+class Program
+{
+    enum Cores
+    {
+        Vermelho,
+        Azul,
+        Amarelo,
+        Verde,
+        Roxo,
+        Laranja
+    }
+
+    static void Main()
+    {
+        int numero = 1;
+
+
+        string resposta = numero switch
+        {
+            > 0 and < 3 => "entre 0 e 3",
+            _ => "Nenhuma das opcoes anteriores"
+        };
+
+        Console.WriteLine(resposta);
+    }
+}
